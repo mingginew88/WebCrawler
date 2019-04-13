29332817b9b0a74893dccd9afcf23b491321fa42
mingginew88 <seoyoungjun88@gmail.com>
2019-04-13 10:38:01 +0900

    no message

--- src/main/java/com/dean/webcrawler/web/StokeController.java
+++ src/main/java/com/dean/webcrawler/web/StokeController.java
@@ -12,8 +12,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class StokeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(StokeController.class);
+	//private static final Logger logger = LoggerFactory.getLogger(StokeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -40,38 +38,44 @@ public class StokeController {
 		String basicUrl = "https://finance.naver.com/sise/sise_market_sum.nhn";
 		
 		// Input page Number
-		int pageNumber = 2;
+		int pageNumber = 1;
 		
-		// Input Data what you want
-		List<String> dataList = new ArrayList<String>();
+		Document doc;
+		
+		// Input DataList what you want
+		List<String> urlList 	= new ArrayList<String>();		// 원하는 urlList
+		List<String> dataList 	= new ArrayList<String>();		// 추출한 기업명List
+		//List<String> codeList 	= new ArrayList<String>();		// 기업 codeList
 		
 		for(int i = 1; i < pageNumber; i++) {
-			
 			String parameter = "?&page=" + pageNumber;
 			
 			String url = basicUrl + parameter;
-						 
+			urlList.add(url);
+		}
+		
+		for(int i = 0; i < urlList.size(); i++) {
 			// Input tag for Crawling 
-			String tag = ".tltle";
+			String nameTag = ".tltle";		// 기업이름
 			
 			// url connect 
-			Document doc = Jsoup.connect(url).get();
+			doc = Jsoup.connect(urlList.get(i)).timeout(500).get();
 			
 			System.out.println("주제 : "+ doc.title());
 			
 			// Elements - doc.select("selector")
-	        Elements elements = doc.select(tag);
-	        
-	        
+	        Elements nameElements = doc.select(nameTag);
 	        
 	        // Test for checking Elements
-	        for(Element element: elements) {
-	            System.out.println(element.text());
+	        for(Element element: nameElements) {
+	            System.out.println(element);
+	            // 파싱 해서 코드번호 가져와 코드리스트에 넣어주기.
+	            //System.out.println(element.select("a").attr("href"));
 	            dataList.add(element.text());
 	        }
-			
 		}
 		
+		// 해당 기업명 들어가서 데이터 가져오기.
         
         model.addAttribute("dataList", dataList);
         
@@ -86,5 +90,11 @@ public class StokeController {
 
         return sdf.format(new Date());
 	}
+	
+	
+	// 전체 html을 파싱하는것 부분부분 가져와서 파싱하는것
+	
+	
+	
 	
 }
